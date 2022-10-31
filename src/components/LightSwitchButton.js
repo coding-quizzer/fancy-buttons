6bd52b2d4c0e17118099a0957fb5f75abc1029f1
William Gadd <wgadd@sasktel.net>
2022-10-31 10:52:08 -0600

    Move the code for toggling the light statement into App.js and delever it to the button through props

--- src/components/LightSwitchButton.js
+++ src/components/LightSwitchButton.js
@@ -1,21 +1,10 @@
 import React from "react";
 
 const LightSwitchButton = function(props) {
-  const { light, setLight } = props
-  
-  const handleClick = () => {
-    if (light === "on") {
-      setLight("off");
-    } else {
-      setLight("on")
-    }
-  };
-
+  const { light, switchLight } = props
   return (
-    <button className="LightSwitchButton" onClick={handleClick}>
-      {/* When the state is on */}
+    <button className="LightSwitchButton" onClick={switchLight}>
       {light === "on" && <span className="on"><i>💡</i> I'm on!</span>}
-      {/* When the state is off */}
       {light === "off" && <span className="off"><i>💡</i> I'm off!</span>}
     </button>
   );
