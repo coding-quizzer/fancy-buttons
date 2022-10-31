d2888f7e7bdefbd0782ca3293dfa85c73a55f02c
William Gadd <wgadd@sasktel.net>
2022-10-31 09:06:30 -0600

    Add starter code for CounterButton component

---
+++ src/components/CounterButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CounterButton = function() {
+  return (
+    <button className="CounterButton">
+      You clicked me X amount of times
+    </button>
+  );
+};
+
+export default CounterButton;
